93734f1c425e96733dea0e8b1df3a9e125cdbbd5
Leonowki <131474580+Leonowki@users.noreply.github.com>
2024-11-21 03:18:17 +0800

    Update main.js

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ const geometry = new THREE.BoxGeometry(2, 2, 2);
 const textureLoader = new THREE.TextureLoader();
 const faceTextures = [];
 for (let i = 1; i <= 6; i++) {
-    const texture = textureLoader.load(`${i}.png`);
+    const texture = textureLoader.load(`text${i}.png`);
     faceTextures.push(new THREE.MeshStandardMaterial({ map: texture }));
 }
 
